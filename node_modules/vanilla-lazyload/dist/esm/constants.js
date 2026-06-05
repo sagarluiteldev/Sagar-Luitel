c9f822c41794616065c10365111df6f805df04b7
Sagar <dragonm0901@gmail.com>
2026-06-05 18:14:10 +0545

    chore: add gitignore, remove node_modules and dist from tracking

--- node_modules/vanilla-lazyload/dist/esm/constants.js
+++
@@ -1,1 +0,0 @@
-const s="src",t="srcset",r="sizes",e="poster",a="llOriginalAttrs",c="data";export{c as DATA,a as ORIGINALS,e as POSTER,r as SIZES,s as SRC,t as SRCSET};
--- node_modules/vanilla-lazyload/dist/esm/tempImage.js
+++
@@ -1,1 +0,0 @@
-const e=e=>{e.llTempImage=document.createElement("IMG")},l=e=>{delete e.llTempImage},m=e=>e.llTempImage;export{e as addTempImage,l as deleteTempImage,m as getTempImage};
