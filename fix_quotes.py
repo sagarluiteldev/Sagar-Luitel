9638b93566d43916352f11aee325dbe30313a5a8
Sagar <dragonm0901@gmail.com>
2026-06-05 18:15:06 +0545

    chore: remove unused python utility scripts

--- fix_quotes.py
+++
@@ -1,7 +0,0 @@
-with open("index.html", "r") as f:
-    content = f.read()
-
-content = content.replace('"; padding: 0; background: none; isolation: isolate;">', '; padding: 0; background: none; isolation: isolate;">')
-
-with open("index.html", "w") as f:
-    f.write(content)
